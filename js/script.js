f031d0a512606f12d926fca89ef36501b002ce28
eighmard18 <aleczeighmard29@gmail.com>
2022-10-11 23:37:30 +0800

    12 - darkmode

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@ jQuery(document).ready(function($) {
     $("#switchmode").change(function () {
         if (this.checked) {
             console.log('checked');
-            $("section").addClass("dark");
-            $('.body-container').attr('style', 'background: #191919 !important');
-            $(".my-icon img.dark").show();
+            $("section, header, footer, button").addClass("dark");
+            $('.body-container').attr('style', 'background: #454545 !important');
+            $(".my-icon img.dark2").show();
             $(".my-icon img.light").hide();
 
         } else if (!this.checked) {
-            $("section").removeClass("dark");
+            $("section, header, footer, button").removeClass("dark");
             $('.body-container').attr('style', 'background: #F6F7FF !important');
-            $(".my-icon img.dark").hide();
+            $(".my-icon img.dark2").hide();
             $(".my-icon img.light").show();
         }
     });
