86df22bbcbe44132061b43b194587f437a6e4bd0
eighmard18 <aleczeighmard29@gmail.com>
2022-10-13 23:58:28 +0800

    skill-section

--- js/script.js
+++ js/script.js
@@ -20,4 +20,30 @@ jQuery(document).ready(function($) {
             $(this).parent().addClass('active').siblings().removeClass('active')    
         })
 
+    AOS.init(); 
+    
+    function animateValue(obj, start, end, duration) {
+        let startTimestamp = null;
+        const step = (timestamp) => {
+            if (!startTimestamp) startTimestamp = timestamp;
+            const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+                obj.innerHTML = Math.floor(progress * (end - start) + start);
+            if (progress < 1) {
+                window.requestAnimationFrame(step);
+            }
+        };
+        window.requestAnimationFrame(step);
+    }
+
+    const obj = document.getElementById("value");
+    animateValue(obj, 0, 80, 3000);
+
+    const obj2 = document.getElementById("value2");
+    animateValue(obj2, 0, 60, 3000);
+
+    const obj3 = document.getElementById("value3");
+    animateValue(obj3, 0, 80, 3000);
+
+    const obj4 = document.getElementById("value4");
+    animateValue(obj4, 0, 50, 3000);
 });
